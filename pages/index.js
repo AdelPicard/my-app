3df9fa334709ffbe399d369446838dcb94d4c050
Picard <pabloaviz74@gmail.com>
2023-11-12 01:08:23 -0300

    nova correção

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,9 @@ function Home () {
     return (
     <div>
         Home
-        <Contador />
     </div>
     )
 }
 
-function Contador () {
-    const [count, setCount] = useState(1)
-
-    function contar () {
-        setCount(count + 1)
-    }
-
-    return (
-        <div>
-            <div>{count}</div>
-            <button onClick={contar}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
